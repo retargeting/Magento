0bdb6b494082219bd5019696d20ad26e5205f284
Cornel Bratu <bratucornel@gmail.com>
2015-06-08 17:31:53 +0300

    Retargeting V3

---
+++ app/code/community/Retargeting/Tracker/controllers/ProductsController.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @category    Retargeting
+ * @package     Retargeting_Tracker
+ * @author      Retargeting <info@retargeting.biz>
+ * @copyright   Copyright (c) Retargeting
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+class Retargeting_Tracker_ProductsController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction() {
+		echo '<?xml version="1.0" encoding="UTF-8"?>';
+		echo '
+		<products>';
+
+		$params = $this->getRequest()->getParams();
+
+        if ( isset($params['key']) && $params['key'] != '' && $params['key'] == Mage::getStoreConfig('retargetingtracker_options/discounts/discount_api_key') ) {
+
+			$collection = Mage::getResourceModel('catalog/product_collection');
+			foreach($collection as $product) {
+				$product = Mage::getModel('catalog/product')->load($product->getId());
+				$product_price = Mage::helper('tax')->getPrice($product, $product->getPrice());
+				$product_promo = ( Mage::helper('tax')->getPrice($product, $product->getPrice()) - Mage::helper('tax')->getPrice($product, $product->getFinalPrice()) > 0 ? Mage::helper('tax')->getPrice($product, $product->getFinalPrice()) : 0 );
+				$product_image = ( $product->getThumbnail() != 'no_selection' ? htmlspecialchars(Mage::helper('catalog/image')->init($product, 'thumbnail')) : htmlspecialchars(Mage::helper('catalog/image')->init($product, 'image')->resize(500)) );
+				$product_url = $product->getProductUrl();
+				echo '
+			<product>
+				<id>'.$product->getId().'</id>
+				<stock>'.$product->getIsInStock().'</stock>
+				<price>'.$product_price.'</price>
+				<promo>'.$product_promo.'</promo>
+				<url>'.$product_url.'</url>
+				<image>'.$product_image.'</image>
+			</product>';
+			}
+		
+		}
+
+		echo '
+		</products>';
+    }
+}
