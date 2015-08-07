baffaee04bbe2ad9afbdfe154b5b77cd97e6d7c6
Retargeting Team <info@retargeting.biz>
2015-08-07 15:16:46 +0300

    Magento 1.4.x compatibility

--- app/code/community/Retargeting/Tracker/Model/Observer.php
+++ app/code/community/Retargeting/Tracker/Model/Observer.php
@@ -9,13 +9,6 @@
 
 class Retargeting_Tracker_Model_Observer
 {
-    /*
-    public function catalogProductLoadAfter(Varien_Event_Observer $observer)
-    {
-        $product = $observer->getEvent()->getProduct();
-    }
-    */
-
     public function TrackSetEmail($observer)
     {
         $event = $observer->getEvent();  //Fetches the current event
@@ -32,7 +25,6 @@ class Retargeting_Tracker_Model_Observer
             $customerCity = $cust_data['city'];
         }
 
-
         $info = array(
             "email" => $customer->getEmail(),
             "name" => $customer->getName(),
@@ -46,6 +38,11 @@ class Retargeting_Tracker_Model_Observer
 
     public function TrackAddToCart($observer)
     {
+    
+        $magentoVersion = Mage::getVersion();
+    
+        if($magentoVersion > "1.4.2.0"){
+
         $helper = Mage::helper('catalog/product_configuration');
 
         $event = $observer->getEvent();  //Fetches the current event
@@ -97,6 +94,22 @@ class Retargeting_Tracker_Model_Observer
         );
 
         Mage::getSingleton('core/session')->setTriggerAddToCart($info);
+        
+        } else {
+            //Magento 1.4 compatibility
+            $helper = Mage::helper('catalog/product');
+
+            $event = $observer->getEvent();  //Fetches the current event
+            $product = $event->getProduct();
+            $quoteItem = $event->getQuoteItem();
+
+            $info = array(
+                "product_id" => $product->getId(),
+                "variation" => "false"
+            );
+
+            Mage::getSingleton('core/session')->setTriggerAddToCart($info);
+        }
     }
 
     public function TrackAddToWishlist($observer)
@@ -126,64 +139,108 @@ class Retargeting_Tracker_Model_Observer
 
     public function TrackSaveOrder($observer)
     {
-        $helper = Mage::helper('catalog/product_configuration');
+        $magentoVersion = Mage::getVersion();
+        if($magentoVersion > "1.4.2.0") {
 
-        $event = $observer->getEvent();  //Fetches the current event
-        $order = $observer->getOrder();
-        $shippingAddress = $order->getShippingAddress();
-        $quote = $observer->getEvent()->getQuote();
+            $helper = Mage::helper('catalog/product_configuration');
 
-        $products = array();
-        foreach ($order->getAllVisibleItems() as $item) {
-            $itemOptions = $item->getProductOptions();
-            $variationCode = "false";
-            $optionsCode = array();
+            $event = $observer->getEvent();  //Fetches the current event
+            $order = $observer->getOrder();
+            $billingAddress = $order->getbillingAddress();
+            $quote = $observer->getEvent()->getQuote();
 
-            $product = Mage::getModel('catalog/product')->load($item->getProductId());
-            if ( $product->isConfigurable() ) {
-                $_optCode = str_replace(' ', '', $item->getSku());
-                $_optCode = str_replace('-', '', $_optCode);
-                $_optCode = strip_tags($_optCode);
-                $optionsCode[] = $_optCode;
-            }
+            $products = array();
+            foreach ($order->getAllVisibleItems() as $item) {
+                $itemOptions = $item->getProductOptions();
+                $variationCode = "false";
+                $optionsCode = array();
 
-            if ( count($itemOptions['options']) > 0 ) {
-                foreach ($itemOptions['options'] as $itemOption) {
-                    $_optCode = str_replace(' ', '', $itemOption['value']);
+                $product = Mage::getModel('catalog/product')->load($item->getProductId());
+                if ( $product->isConfigurable() ) {
+                    $_optCode = str_replace(' ', '', $item->getSku());
                     $_optCode = str_replace('-', '', $_optCode);
                     $_optCode = strip_tags($_optCode);
                     $optionsCode[] = $_optCode;
                 }
+
+                if ( count($itemOptions['options']) > 0 ) {
+                    foreach ($itemOptions['options'] as $itemOption) {
+                        $_optCode = str_replace(' ', '', $itemOption['value']);
+                        $_optCode = str_replace('-', '', $_optCode);
+                        $_optCode = strip_tags($_optCode);
+                        $optionsCode[] = $_optCode;
+                    }
+                }
+
+                $variationCode = count($optionsCode) > 0 ? '"'.implode('-', $optionsCode).'"' : "false";
+
+                $products[] = '{
+                    "id": "'. $item->getProductId() .'",
+                    "quantity": '. $item->getQtyOrdered() .',
+                    "price": ' . Mage::helper('tax')->getPrice($item, $item->getPrice()).',
+                    "variation_code": ' . $variationCode . '}';
             }
 
-            $variationCode = count($optionsCode) > 0 ? '"'.implode('-', $optionsCode).'"' : "false";
+            $info = array(
+                "order_no" => $order->getIncrementId(),
+                "firstname" => $billingAddress->getFirstname(),
+                "lastname" => $billingAddress->getLastname(),
+                "email" => $billingAddress->getEmail(),
+                "phone" => $billingAddress->getTelephone(),
+                "state" => $billingAddress->getRegion(),
+                "city" => $billingAddress->getCity(),
+                "address" => implode(" ", $billingAddress->getStreet()),
+                "data" => $billingAddress->getData(),
+                "discount" => $order->getDiscountAmount(),
+                "discount_code" => $order->getCouponCode(),
+                "shipping" => $order->getShippingInclTax(),
+                "total" => $order->getGrandTotal(),
+                "products" => "[".implode(",", $products)."]",
+            );
+            Mage::getSingleton('core/session')->setTriggerSaveOrder($info);
+        } else {
+           
+           // Magento 1.4 compatibility
+            $helper = Mage::helper('catalog/product');
 
-            $products[] = '{
-                "id": "'. $item->getProductId() .'",
-                "quantity": '. $item->getQtyOrdered() .',
-                "price": ' . Mage::helper('tax')->getPrice($item, $item->getPrice()).',
-                "variation_code": ' . $variationCode . '}';
+            $event = $observer->getEvent();  //Fetches the current event
+            $order = $observer->getOrder();
+            $billingAddress = $order->getBillingAddress();
+            $quote = $observer->getEvent()->getQuote();
+
+            $products = array();
+            foreach ($order->getAllVisibleItems() as $item) {
+                $itemOptions = $item->getProductOptions();
+
+                $product = Mage::getModel('catalog/product')->load($item->getProductId());
+
+                $variationCode = "";
+
+                $products[] = '{
+                    "id": "'. $item->getProductId() .'",
+                    "quantity": '. $item->getQtyOrdered() .',
+                    "price": ' . Mage::helper('tax')->getPrice($item, $item->getPrice()).',
+                    "variation_code": false}';
+            }
+
+            $info = array(
+                "order_no" => $order->getIncrementId(),
+                "firstname" => $billingAddress->getFirstname(),
+                "lastname" => $billingAddress->getLastname(),
+                "email" => $billingAddress->getEmail(),
+                "phone" => $billingAddress->getTelephone(),
+                "state" => $billingAddress->getRegion(),
+                "city" => $billingAddress->getCity(),
+                "address" => implode(" ", $billingAddress->getStreet()),
+                "data" => $billingAddress->getData(),
+                "discount" => $order->getDiscountAmount(),
+                "discount_code" => $order->getCouponCode(),
+                "shipping" => $order->getShippingInclTax(),
+                "total" => $order->getGrandTotal(),
+                "products" => "[".implode(",", $products)."]",
+            );
+
+            Mage::getSingleton('core/session')->setTriggerSaveOrder($info);
         }
-
-        $info = array(
-            "order_no" => $order->getIncrementId(),
-            "firstname" => $shippingAddress->getFirstname(),
-            "lastname" => $shippingAddress->getLastname(),
-            "email" => $shippingAddress->getEmail(),
-            "phone" => $shippingAddress->getTelephone(),
-            "state" => $shippingAddress->getRegion(),
-            "city" => $shippingAddress->getCity(),
-            "address" => implode(" ", $shippingAddress->getStreet()),
-            "data" => $shippingAddress->getData(),
-            "discount" => $order->getDiscountAmount(),
-            "discount_code" => '',
-            "shipping" => $order->getShippingInclTax(),
-            "total" => $order->getGrandTotal(),
-            "products" => "[".implode(",", $products)."]",
-        );
-
-        Mage::log($info, null, 'retargeting.log');
-
-        Mage::getSingleton('core/session')->setTriggerSaveOrder($info);
     }
 }
