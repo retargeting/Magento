49939ec3effdeb8f4529c5329d13991d8eade82e
CarolPelu <pelucaroltheo@gmail.com>
2017-06-19 16:25:21 +0300

    init

--- app/code/community/Retargeting/Tracker/Helper/Data.php
+++ app/code/community/Retargeting/Tracker/Helper/Data.php
@@ -38,5 +38,51 @@ class Retargeting_Tracker_Helper_Data extends Mage_Core_Helper_Abstract
     {
         return Mage::getStoreConfig(self::XML_PATH_IMAGECLASS, $store);
     }
+    /*
+    *   Formats the price to have 2 decimals
+    */
+    protected function formatter($price)
+    {
+        return number_format($price, 2);
+    }
+    
+    /*
+    *   TBD
+    */
+    public function preparePrice(Mage_Catalog_Model_Product $product)
+    {
+        $price = 0;
+        switch ($product->getTypeId()) {
+            case Mage_Catalog_Model_Product_Type::TYPE_BUNDLE:
+                $bundlePrices = $product->getPriceModel();
+                $specialPrice = $bundlePrices->getTotalPrices($product, 'min', true);
+                $maxPrice = $bundlePrices->getTotalPrices($product, 'max', true);
+                break;
+            case Mage_Catalog_Model_Product_Type::TYPE_GROUPED:
+                $typeInstance = $product->getTypeInstance();
+                $associatedProducts = $typeInstance->setStoreFilter($product->getStore(),
+                    $product)->getAssociatedProducts($product);
+                $cheapestAssociatedProduct = null;
+                $minimalPrice = 0;
+                foreach ($associatedProducts as $associatedProduct) {
+                    $temp = $associatedProduct->getSpecialPrice();
+                    if ($minimalPrice === 0 || $minimalPrice > $temp) {
+                        $minimalPrice = $temp;
+                        $cheapestAssociatedProduct = $associatedProduct;
+                    }
+                }
+                $specialPrice = $minimalPrice;
+                if ($cheapestAssociatedProduct) {
+                    $helper = Mage::helper('tax');
+                    $specialPrice = $helper->getPrice($cheapestAssociatedProduct, $specialPrice, true);  
+                }
+                // Trebuie sa gasesc o metoda prin care sa iau cel mai mare base price al produsului
+                // de pus intr-un branch separat
+            default:
+                $price = 'pretdefault';
+                break;  
+        }
+        return $price;
+    }
 
 }
